3f6196f30d9ed86929f139d6e4294a8b6487f37b
Aagam Jain <aagam.jain@captainfresh.com>
2024-11-04 17:16:52 +0530

    remove generics and added update cms controller

--- src/main/java/org/trips/service_framework/controllers/BaseCmsController.java
+++ src/main/java/org/trips/service_framework/controllers/BaseCmsController.java
@@ -1,35 +1,49 @@
 package org.trips.service_framework.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.trips.service_framework.aop.Authenticate;
 import org.trips.service_framework.dtos.CmsSearchRequestBody;
 import org.trips.service_framework.dtos.CmsSkuResponse.Sku;
 import org.trips.service_framework.dtos.SkuAttributes;
 import org.trips.service_framework.helpers.CmsHelper;
-import org.trips.service_framework.models.responses.BaseResponse;
+import org.trips.service_framework.models.responses.StatusResponse;
+import org.trips.service_framework.responses.CmsResponse;
 import org.trips.service_framework.services.CmsService;
 
 import java.util.*;
 
+@RestController
+@RequestMapping("/cms")
 @RequiredArgsConstructor
-public abstract class BaseCmsController<R extends BaseResponse> {
+public class BaseCmsController {
     protected final CmsService cmsService;
     protected final CmsHelper cmsHelper;
 
-    protected abstract R createResponse(List<Sku> skuList);
+    protected CmsResponse createResponse(List<Sku> skuList) {
+        return CmsResponse.builder()
+                .data(skuList)
+                .status(StatusResponse.builder()
+                        .statusCode(HttpStatus.SC_OK)
+                        .statusType(StatusResponse.Type.SUCCESS)
+                        .totalCount(skuList.size())
+                        .build())
+                .build();
+    }
 
     @Authenticate
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public R skuSearch(@RequestBody CmsSearchRequestBody body) {
+    public CmsResponse skuSearch(@RequestBody CmsSearchRequestBody body) {
         return createResponse(cmsService.skuSearch(body));
     }
 
     @Authenticate
     @RequestMapping(value = "/find-or-create", method = RequestMethod.POST)
-    public R lookupOrCreateSku(@RequestBody SkuAttributes body) {
+    public CmsResponse lookupOrCreateSku(@RequestBody SkuAttributes body) {
         return createResponse(Collections.singletonList(cmsService.getOrCreateSku(body)));
     }
 }
